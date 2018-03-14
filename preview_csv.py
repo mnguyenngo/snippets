6cbd20f1051dc58591290ce7164767ecfd7e8777
Nguyen Ngo <mnguyenngo@gmail.com>
2018-03-14 14:40:28 -0700

    Add preview_csv script

---
+++ preview_csv.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import sys
+
+def preview_csv(file, delimiter=','):
+    data = pd.read_csv(sys.argv[1], delimiter=delimiter)
+
+    print('-------------')
+    print('DESCRIBE')
+    print('-------------')
+    print(data.describe())
+    print('-------------')
+    print('INFO')
+    print('-------------')
+    print(data.info())
+    print('-------------')
+    print('COLUMNS')
+    print('-------------')
+    print(data.columns)
+    print('-------------')
+    print('HEAD')
+    print('-------------')
+    print(data.head())
+    print('-------------')
+    print('TAIL')
+    print('-------------')
+    print(data.tail())
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        preview_csv(sys.argv[1], sys.argv[2])
+    else:
+        preview_csv(sys.argv[1], ',')
